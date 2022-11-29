a81d8b1edc19c1d9a2af36d4e7fd933e60391171
Maximilian <Maximilian.Bornstaedt@stud.srh-campus-berlin.de>
2022-11-29 17:56:53 +0100

    Debugging start menu

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,5 @@ public class Main {
     public static void main(String[] args) {
         int action = startMenu.printMenu();
         System.out.println("Success: user selected valid option " + action + " in the start menu");
-    }
+        }
 }
