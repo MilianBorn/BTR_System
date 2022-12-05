b988ba81f6fba2b37ca7e40404c9cec56e5c8088
Maximilian <Maximilian.Bornstaedt@stud.srh-campus-berlin.de>
2022-12-05 18:46:13 +0100

    Added some comments to the user class

--- src/Peaces/User.java
+++ src/Peaces/User.java
@@ -1,10 +1,12 @@
 package Peaces;
 
-public class User {
+public class User { // this class represents a user and is used to initialise user instances
     public String fname;
     public String lname;
     public String dob;
     public String email;
     public String username;
     public String password;
+
+    // ToDo: add all elements relevant to the user class (e.g. array list holding the tickets)
 }
